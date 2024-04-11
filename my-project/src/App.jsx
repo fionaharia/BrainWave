9ae095faa07885e30bb8d75059c4a5d3170fefea
Fiona Haria <hariafiona@gmail.com>
2024-04-12 00:31:45 +0530

    initial commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,25 @@
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.5rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+}
+
+export default App;
